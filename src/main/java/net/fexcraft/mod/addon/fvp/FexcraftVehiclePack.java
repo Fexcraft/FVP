7c22573c11ba9e00295fea3b969871e20dbea800
Ferdinand <fercalo96@yahoo.de>
2019-04-22 17:59:00 +0200

    Basic Files.

---
+++ src/main/java/net/fexcraft/mod/addon/fvp/FexcraftVehiclePack.java
@@ -0,0 +1,8 @@
+package net.fexcraft.mod.addon.fvp;
+
+import net.fexcraft.mod.fvtm.data.AddonClass;
+import net.minecraftforge.fml.common.Mod;
+
+@AddonClass(registryname = "fvtm:fvp")
+@Mod(modid = "fvp", name = "Fex's Vehicle Pack", version = "2.0", useMetadata = true, dependencies = "required-after:fcl;required-after:fvtm")
+public class FexcraftVehiclePack {}
