cf92a219b86d05cd14ef0bf66b7264cf5b7cc8c8
Ferdinand Calo <fercalo96@yahoo.de>
2021-08-21 21:57:55 +0200

    Update T1SnowPlow.java

--- src/main/java/net/fexcraft/mod/addon/fvp/models/part/t1/T1SnowPlow.java
+++ src/main/java/net/fexcraft/mod/addon/fvp/models/part/t1/T1SnowPlow.java
@@ -2,12 +2,9 @@ package net.fexcraft.mod.addon.fvp.models.part.t1;
 
 import net.fexcraft.lib.mc.api.registry.fModel;
 import net.fexcraft.lib.tmt.ModelRendererTurbo;
-import net.fexcraft.mod.fvtm.data.attribute.Attribute;
-import net.fexcraft.mod.fvtm.data.root.RenderCache;
-import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
+import net.fexcraft.mod.fvtm.model.DefaultPrograms;
 import net.fexcraft.mod.fvtm.model.PartModel;
 import net.fexcraft.mod.fvtm.model.TurboList;
-import net.minecraft.entity.Entity;
 
 @fModel(registryname = "fvp:models/part/t1_snow_plow")
 public class T1SnowPlow extends PartModel {
@@ -17,68 +14,71 @@ public class T1SnowPlow extends PartModel {
     public T1SnowPlow(){
     	super(); textureX = 512; textureY = 128;
         addToCreators("Ferdinand (FEX___96)");
-        ModelRendererTurbo[] body = new ModelRendererTurbo[1];
-        body[0] = new ModelRendererTurbo(this, 185, 1, textureX, textureY); // Box 9
-        body[0].addShapeBox(0F, 0F, -24F, 4, 7, 48, 0F, 0F, 0F, 0F, 0F, 0F, -1F, 0F, 0F, -1F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -1F, -1F, 0F, -1F, -1F, 0F, 0F, 0F); // Box 9
-        body[0].setRotationPoint(56F, -4.5F, 0F);
-        this.add("body", body); this.body = this.get("body");
-        //
-        ModelRendererTurbo[] snowplow = new ModelRendererTurbo[13];
-        snowplow[0] = new ModelRendererTurbo(this, 1, 1, textureX, textureY); // Box 0
-        snowplow[1] = new ModelRendererTurbo(this, 121, 1, textureX, textureY); // Box 1
-        snowplow[2] = new ModelRendererTurbo(this, 241, 1, textureX, textureY); // Box 2
-        snowplow[3] = new ModelRendererTurbo(this, 361, 1, textureX, textureY); // Box 3
-        snowplow[4] = new ModelRendererTurbo(this, 1, 65, textureX, textureY); // Box 4
-        snowplow[5] = new ModelRendererTurbo(this, 1, 1, textureX, textureY); // Box 5
-        snowplow[6] = new ModelRendererTurbo(this, 65, 1, textureX, textureY); // Box 6
-        snowplow[7] = new ModelRendererTurbo(this, 105, 1, textureX, textureY); // Box 7
-        snowplow[8] = new ModelRendererTurbo(this, 137, 1, textureX, textureY); // Box 8
-        snowplow[9] = new ModelRendererTurbo(this, 33, 1, textureX, textureY); // Box 10
-        snowplow[10] = new ModelRendererTurbo(this, 185, 1, textureX, textureY); // Box 11
-        snowplow[11] = new ModelRendererTurbo(this, 201, 1, textureX, textureY); // Box 12
-        snowplow[12] = new ModelRendererTurbo(this, 217, 1, textureX, textureY); // Box 13
-        snowplow[0].addShapeBox(5F, -3F, -27.5F, 2, 6, 56, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 5F, 0F, 0F, -5F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 5F, 0F, 0F, -5F, 0F, 0F); // Box 0
-        snowplow[0].setRotationPoint(58F, -5F, 0F);
-        snowplow[1].addShapeBox(5F, 3F, -27.5F, 2, 6, 56, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 5F, 0F, 0F, -5F, 0F, 0F, -1F, 0F, 0F, 1F, 0F, 0F, 6F, 0F, 0F, -6F, 0F, 0F); // Box 1
-        snowplow[1].setRotationPoint(58F, -5F, 0F);
-        snowplow[2].addShapeBox(6F, 9F, -27.5F, 2, 6, 56, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 5F, 0F, 0F, -5F, 0F, 0F, -4F, 0F, 0F, 4F, 0F, 0F, 10F, 0F, 0F, -10F, 0F, 0F); // Box 2
-        snowplow[2].setRotationPoint(58F, -5F, 0F);
-        snowplow[3].addShapeBox(5F, -6F, -27.5F, 2, 3, 56, 0F, -2F, 0F, 0F, 2F, 0F, 0F, 7F, 0F, 0F, -7F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 5F, 0F, 0F, -5F, 0F, 0F); // Box 3
-        snowplow[3].setRotationPoint(58F, -5F, 0F);
-        snowplow[4].addShapeBox(10.5F, 14F, -28F, 1, 1, 57, 0F, 0F, 0F, -0.3F, 0F, 0F, 0F, 6F, 0F, 0F, -6F, 0F, -0.3F, 0F, 0F, -0.3F, 1F, 0F, 0F, 7F, 0F, 0F, -6F, 0F, -0.3F); // Box 4
-        snowplow[4].setRotationPoint(58F, -5F, 0F);
-        snowplow[5].addBox(-2F, 4F, -13.5F, 10, 2, 4, 0F); // Box 5
-        snowplow[5].setRotationPoint(58F, -5F, 0F);
-        snowplow[6].addBox(-2F, 4F, 10.5F, 12, 2, 4, 0F); // Box 6
-        snowplow[6].setRotationPoint(58F, -5F, 0F);
-        snowplow[7].addShapeBox(-2F, -2F, 17.5F, 12, 2, 3, 0F, 0F, 0F, 0F, 0F, 0F, -3F, 0F, 0F, 3F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -3F, 0F, 0F, 3F, 0F, 0F, 0F); // Box 7
-        snowplow[7].setRotationPoint(58F, -5F, 0F);
-        snowplow[8].addShapeBox(-2F, -3F, -20.5F, 9, 2, 3, 0F, 0F, 0F, 0F, 0F, 0F, 3F, 0F, 0F, -3F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 3F, 0F, 0F, -3F, 0F, 0F, 0F); // Box 8
-        snowplow[8].setRotationPoint(58F, -5F, 0F);
-        snowplow[9].addShapeBox(12F, -8F, 21.5F, 1, 2, 4, 0F, 0F, -0.5F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -0.5F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F); // Box 10
-        snowplow[9].setRotationPoint(58F, -5F, 0F);
-        snowplow[10].addShapeBox(8F, -8F, -24.5F, 1, 2, 4, 0F, 0F, -0.5F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -0.5F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F); // Box 11
-        snowplow[10].setRotationPoint(58F, -5F, 0F);
-        snowplow[11].addShapeBox(13F, -8F, 21.5F, 1, 2, 4, 0F, 0F, -0.2F, -0.2F, -0.8F, -0.45F, -0.45F, -0.8F, -0.45F, -0.45F, 0F, -0.2F, -0.2F, 0F, -0.2F, -0.2F, -0.8F, -0.45F, -0.45F, -0.8F, -0.45F, -0.45F, 0F, -0.2F, -0.2F); // Box 12
-        snowplow[11].setRotationPoint(58F, -5F, 0F);
-        snowplow[12].addShapeBox(9F, -8F, -24.5F, 1, 2, 4, 0F, 0F, -0.2F, -0.2F, -0.8F, -0.45F, -0.45F, -0.8F, -0.45F, -0.45F, 0F, -0.2F, -0.2F, 0F, -0.2F, -0.2F, -0.8F, -0.45F, -0.45F, -0.8F, -0.45F, -0.45F, 0F, -0.2F, -0.2F); // Box 13
-        snowplow[12].setRotationPoint(58F, -5F, 0F);
-        this.add("snowplow", snowplow); this.snowplow = this.get("snowplow");
-    }
-
-    @Override
-    public void render(VehicleData data, String us){
-        body.render(null, data, data, us, null);
-        snowplow.rotate(0, 0, 20f, true);
-        snowplow.render(null, data, data, us, null);
-    }
-
-    @Override
-    public void render(VehicleData data, String us, Entity vehicle, RenderCache cache){
-        body.render(null, data, data, us, cache);
-        Attribute<?> attr = data.getAttribute("t1_snowplow_state");
-        snowplow.rotate(0, 0, attr == null ? 0 : attr.conditional_boolean(0f, 20f), true);
-        snowplow.render(null, data, data, us, cache);
+        
+		TurboList lights = new TurboList("lights");
+		lights.add(new ModelRendererTurbo(lights, 201, 1, textureX, textureY)
+			.addShapeBox(13, -8, 21.5f, 1, 2, 4, 0, 0, -0.2f, -0.2f, -0.8f, -0.45f, -0.45f, -0.8f, -0.45f, -0.45f, 0, -0.2f, -0.2f, 0, -0.2f, -0.2f, -0.8f, -0.45f, -0.45f, -0.8f, -0.45f, -0.45f, 0, -0.2f, -0.2f)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 12")
+		);
+		lights.add(new ModelRendererTurbo(lights, 217, 1, textureX, textureY)
+			.addShapeBox(9, -8, -24.5f, 1, 2, 4, 0, 0, -0.2f, -0.2f, -0.8f, -0.45f, -0.45f, -0.8f, -0.45f, -0.45f, 0, -0.2f, -0.2f, 0, -0.2f, -0.2f, -0.8f, -0.45f, -0.45f, -0.8f, -0.45f, -0.45f, 0, -0.2f, -0.2f)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 13")
+		);
+		lights.addPrograms(DefaultPrograms.LIGHTS, new DefaultPrograms.AttributeRotator("t1_snowplow_state", true, 0, 20, 1, 2, -20f));
+		this.groups.add(lights);
+		//
+		TurboList base = new TurboList("base");
+		base.add(new ModelRendererTurbo(base, 185, 1, textureX, textureY)
+			.addShapeBox(0, 0, -24, 4, 7, 48, 0, 0, 0, 0, 0, 0, -1, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, -1, -1, 0, -1, -1, 0, 0, 0)
+			.setRotationPoint(56, -4.5f, 0).setRotationAngle(0, 0, -0).setName("Box 9")
+		);
+		this.groups.add(base);
+		//
+		TurboList moving = new TurboList("moving");
+		moving.add(new ModelRendererTurbo(moving, 1, 1, textureX, textureY)
+			.addShapeBox(5, -3, -27.5f, 2, 6, 56, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, -5, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, -5, 0, 0)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 0")
+		);
+		moving.add(new ModelRendererTurbo(moving, 121, 1, textureX, textureY)
+			.addShapeBox(5, 3, -27.5f, 2, 6, 56, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, -5, 0, 0, -1, 0, 0, 1, 0, 0, 6, 0, 0, -6, 0, 0)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 1")
+		);
+		moving.add(new ModelRendererTurbo(moving, 241, 1, textureX, textureY)
+			.addShapeBox(6, 9, -27.5f, 2, 6, 56, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, -5, 0, 0, -4, 0, 0, 4, 0, 0, 10, 0, 0, -10, 0, 0)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 2")
+		);
+		moving.add(new ModelRendererTurbo(moving, 361, 1, textureX, textureY)
+			.addShapeBox(5, -6, -27.5f, 2, 3, 56, 0, -2, 0, 0, 2, 0, 0, 7, 0, 0, -7, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, -5, 0, 0)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 3")
+		);
+		moving.add(new ModelRendererTurbo(moving, 1, 65, textureX, textureY)
+			.addShapeBox(10.5f, 14, -28, 1, 1, 57, 0, 0, 0, -0.3f, 0, 0, 0, 6, 0, 0, -6, 0, -0.3f, 0, 0, -0.3f, 1, 0, 0, 7, 0, 0, -6, 0, -0.3f)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 4")
+		);
+		moving.add(new ModelRendererTurbo(moving, 1, 1, textureX, textureY).addBox(-2, 4, -13.5f, 10, 2, 4)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 5")
+		);
+		moving.add(new ModelRendererTurbo(moving, 65, 1, textureX, textureY).addBox(-2, 4, 10.5f, 12, 2, 4)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 6")
+		);
+		moving.add(new ModelRendererTurbo(moving, 105, 1, textureX, textureY)
+			.addShapeBox(-2, -2, 17.5f, 12, 2, 3, 0, 0, 0, 0, 0, 0, -3, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, -3, 0, 0, 3, 0, 0, 0)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 7")
+		);
+		moving.add(new ModelRendererTurbo(moving, 137, 1, textureX, textureY)
+			.addShapeBox(-2, -3, -20.5f, 9, 2, 3, 0, 0, 0, 0, 0, 0, 3, 0, 0, -3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, -3, 0, 0, 0)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 8")
+		);
+		moving.add(new ModelRendererTurbo(moving, 33, 1, textureX, textureY)
+			.addShapeBox(12, -8, 21.5f, 1, 2, 4, 0, 0, -0.5f, 0, 0, 0, 0, 0, 0, 0, 0, -0.5f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 10")
+		);
+		moving.add(new ModelRendererTurbo(moving, 185, 1, textureX, textureY)
+			.addShapeBox(8, -8, -24.5f, 1, 2, 4, 0, 0, -0.5f, 0, 0, 0, 0, 0, 0, 0, 0, -0.5f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+			.setRotationPoint(58, -5, 0).setRotationAngle(0, 0, -0).setName("Box 11")
+		);
+		moving.addProgram(new DefaultPrograms.AttributeRotator("t1_snowplow_state", true, 0, 20, 1, 2, -20f));
+		this.groups.add(moving);
     }
 
 }
