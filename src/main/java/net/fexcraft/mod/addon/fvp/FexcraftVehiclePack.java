8418e72d743a02a53c8f43b170e3b51e261d6d19
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-14 21:57:17 +0200

    Removal of Forge, Gradle and Java code.

--- src/main/java/net/fexcraft/mod/addon/fvp/FexcraftVehiclePack.java
+++
@@ -1,8 +0,0 @@
-package net.fexcraft.mod.addon.fvp;
-
-import net.fexcraft.mod.fvtm.data.addon.AddonClass;
-import net.minecraftforge.fml.common.Mod;
-
-@AddonClass(registryname = "fvtm:fvp")
-@Mod(modid = "fvp", name = "Fex's Vehicle Pack", version = "2.7.1", useMetadata = true, dependencies = "required-after:fcl;required-after:fvtm")
-public class FexcraftVehiclePack {}
