1cee9dc34769fc347ba3e7e39d0013893f54f0e7
Ferdinand Calo <fexcalo@gmail.com>
2023-11-14 01:04:08 +0100

    Update C11Interior.java

--- src/main/java/net/fexcraft/mod/addon/fvp/models/part/c11/C11Interior.java
+++ src/main/java/net/fexcraft/mod/addon/fvp/models/part/c11/C11Interior.java
@@ -6,7 +6,6 @@ import net.fexcraft.lib.tmt.ModelRendererTurbo;
 import net.fexcraft.mod.fvtm.data.attribute.Attribute;
 import net.fexcraft.mod.fvtm.model.DefaultPrograms;
 import net.fexcraft.mod.fvtm.model.ModelGroup;
-import net.fexcraft.mod.fvtm.model.ModelRenderData;
 import net.fexcraft.mod.fvtm.model.PartModel;
 
 /** This file was exported via the FVTM Exporter V1.4 of<br>
